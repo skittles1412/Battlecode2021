e048ab65ffc2a8943ec85af7a8a2d86c2b6047bc
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2021-01-14 11:27:12 -0800

    Move muckrakers towards slanderers

--- src/sprint_0/Muckraker.java
+++ src/sprint_0/Muckraker.java
@@ -21,11 +21,15 @@ public class Muckraker {
 			for(int i = nearbyRobots.length; --i>=0; ) {
 				RobotInfo robotInfo = nearbyRobots[i];
 				MapLocation mapLocation = robotInfo.location;
-				if(robotInfo.type==RobotType.ENLIGHTENMENT_CENTER) {
-					flag += 35-myLocation.distanceSquaredTo(mapLocation);
+				switch(robotInfo.type) {
+					case ENLIGHTENMENT_CENTER:
+						flag += 25*(40-myLocation.distanceSquaredTo(mapLocation));
+						break;
+					case MUCKRAKER:
+						flag += 10*(35-myLocation.distanceSquaredTo(mapLocation));
 				}
 			}
-			robotController.setFlag(flag*100);
+			robotController.setFlag(flag);
 			return;
 		}
 
@@ -43,14 +47,14 @@ public class Muckraker {
 			MapLocation location6 = robotController.adjacentLocation(Direction.WEST);
 			MapLocation location7 = robotController.adjacentLocation(Direction.NORTHWEST);
 			MapLocation location8 = robotController.getLocation();
-			double cost0 = robotController.canMove(Direction.NORTH) ? 1.6/robotController.sensePassability(location0) : 1e10;
-			double cost1 = robotController.canMove(Direction.NORTHEAST) ? 1.6/robotController.sensePassability(location1) : 1e10;
-			double cost2 = robotController.canMove(Direction.EAST) ? 1.6/robotController.sensePassability(location2) : 1e10;
-			double cost3 = robotController.canMove(Direction.SOUTHEAST) ? 1.6/robotController.sensePassability(location3) : 1e10;
-			double cost4 = robotController.canMove(Direction.SOUTH) ? 1.6/robotController.sensePassability(location4) : 1e10;
-			double cost5 = robotController.canMove(Direction.SOUTHWEST) ? 1.6/robotController.sensePassability(location5) : 1e10;
-			double cost6 = robotController.canMove(Direction.WEST) ? 1.6/robotController.sensePassability(location6) : 1e10;
-			double cost7 = robotController.canMove(Direction.NORTHWEST) ? 1.6/robotController.sensePassability(location7) : 1e10;
+			double cost0 = robotController.canMove(Direction.NORTH) ? 1.5/robotController.sensePassability(location0) : 1e10;
+			double cost1 = robotController.canMove(Direction.NORTHEAST) ? 1.5/robotController.sensePassability(location1) : 1e10;
+			double cost2 = robotController.canMove(Direction.EAST) ? 1.5/robotController.sensePassability(location2) : 1e10;
+			double cost3 = robotController.canMove(Direction.SOUTHEAST) ? 1.5/robotController.sensePassability(location3) : 1e10;
+			double cost4 = robotController.canMove(Direction.SOUTH) ? 1.5/robotController.sensePassability(location4) : 1e10;
+			double cost5 = robotController.canMove(Direction.SOUTHWEST) ? 1.5/robotController.sensePassability(location5) : 1e10;
+			double cost6 = robotController.canMove(Direction.WEST) ? 1.5/robotController.sensePassability(location6) : 1e10;
+			double cost7 = robotController.canMove(Direction.NORTHWEST) ? 1.5/robotController.sensePassability(location7) : 1e10;
 			double cost8 = robotController.sensePassability(location8);
 			RobotInfo[] nearbyRobots = robotController.senseNearbyRobots(-1, robotController.getTeam().opponent());
 			int id = 0, influence = -1;
@@ -59,21 +63,30 @@ public class Muckraker {
 				MapLocation mapLocation = robotInfo.location;
 				switch(robotInfo.type) {
 					case ENLIGHTENMENT_CENTER:
-						cost0 += 100*mapLocation.distanceSquaredTo(location0);
-						cost1 += 100*mapLocation.distanceSquaredTo(location1);
-						cost2 += 100*mapLocation.distanceSquaredTo(location2);
-						cost3 += 100*mapLocation.distanceSquaredTo(location3);
-						cost4 += 100*mapLocation.distanceSquaredTo(location4);
-						cost5 += 100*mapLocation.distanceSquaredTo(location5);
-						cost6 += 100*mapLocation.distanceSquaredTo(location6);
-						cost7 += 100*mapLocation.distanceSquaredTo(location7);
-						cost8 += 100*mapLocation.distanceSquaredTo(location8);
+						cost0 += 25*mapLocation.distanceSquaredTo(location0);
+						cost1 += 25*mapLocation.distanceSquaredTo(location1);
+						cost2 += 25*mapLocation.distanceSquaredTo(location2);
+						cost3 += 25*mapLocation.distanceSquaredTo(location3);
+						cost4 += 25*mapLocation.distanceSquaredTo(location4);
+						cost5 += 25*mapLocation.distanceSquaredTo(location5);
+						cost6 += 25*mapLocation.distanceSquaredTo(location6);
+						cost7 += 25*mapLocation.distanceSquaredTo(location7);
+						cost8 += 25*mapLocation.distanceSquaredTo(location8);
 						break;
 					case SLANDERER:
 						if(location8.distanceSquaredTo(mapLocation)<=12&&robotInfo.influence>influence) {
 							id = robotInfo.ID;
 							influence = robotInfo.influence;
 						}
+						cost0 += 10*mapLocation.distanceSquaredTo(location0);
+						cost1 += 10*mapLocation.distanceSquaredTo(location1);
+						cost2 += 10*mapLocation.distanceSquaredTo(location2);
+						cost3 += 10*mapLocation.distanceSquaredTo(location3);
+						cost4 += 10*mapLocation.distanceSquaredTo(location4);
+						cost5 += 10*mapLocation.distanceSquaredTo(location5);
+						cost6 += 10*mapLocation.distanceSquaredTo(location6);
+						cost7 += 10*mapLocation.distanceSquaredTo(location7);
+						cost8 += 10*mapLocation.distanceSquaredTo(location8);
 				}
 			}
 			if(id!=0) {
@@ -89,15 +102,15 @@ public class Muckraker {
 //						for(int j = 8; --j>=0; ) {
 //							cost[j] -= 7*mapLocation.distanceSquaredTo(locations[j]);
 //						}
-						cost0 -= 2.3*mapLocation.distanceSquaredTo(location0);
-						cost1 -= 2.3*mapLocation.distanceSquaredTo(location1);
-						cost2 -= 2.3*mapLocation.distanceSquaredTo(location2);
-						cost3 -= 2.3*mapLocation.distanceSquaredTo(location3);
-						cost4 -= 2.3*mapLocation.distanceSquaredTo(location4);
-						cost5 -= 2.3*mapLocation.distanceSquaredTo(location5);
-						cost6 -= 2.3*mapLocation.distanceSquaredTo(location6);
-						cost7 -= 2.3*mapLocation.distanceSquaredTo(location7);
-						cost8 -= 2.3*mapLocation.distanceSquaredTo(location8);
+						cost0 -= 2.5*mapLocation.distanceSquaredTo(location0);
+						cost1 -= 2.5*mapLocation.distanceSquaredTo(location1);
+						cost2 -= 2.5*mapLocation.distanceSquaredTo(location2);
+						cost3 -= 2.5*mapLocation.distanceSquaredTo(location3);
+						cost4 -= 2.5*mapLocation.distanceSquaredTo(location4);
+						cost5 -= 2.5*mapLocation.distanceSquaredTo(location5);
+						cost6 -= 2.5*mapLocation.distanceSquaredTo(location6);
+						cost7 -= 2.5*mapLocation.distanceSquaredTo(location7);
+						cost8 -= 2.5*mapLocation.distanceSquaredTo(location8);
 						break;
 					case MUCKRAKER:
 //						for(int j = 8; --j>=0; ) {
