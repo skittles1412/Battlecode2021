625561ab5450eb2bbb97ee81cf9cad1cf6e3a469
qpwoeirut <stanleyzhong8@gmail.com>
2021-01-15 21:06:35 -0800

    bad strategy

--- src/antimuckrush/EnlightenmentCenter.java
+++ src/antimuckrush/EnlightenmentCenter.java
@@ -13,7 +13,7 @@ public class EnlightenmentCenter {
     public static final Direction[] sortedDirections = {Direction.NORTH, Direction.NORTHEAST, Direction.EAST, Direction.SOUTHEAST, Direction.SOUTH, Direction.SOUTHWEST, Direction.WEST, Direction.NORTHWEST};
     private static int lastPoliSpawn = 0;
 
-    public static void initialize(RobotController robotController) {
+    public static void initialize(RobotController robotController) throws GameActionException {
         EnlightenmentCenter.robotController = robotController;
         double[] val = new double[8];
         MapLocation myLocation = robotController.getLocation();
@@ -24,22 +24,69 @@ public class EnlightenmentCenter {
             }
         }
         Arrays.sort(sortedDirections, Comparator.comparingDouble(o -> val[o.ordinal()]));
+        robotController.setFlag(50 << 18);
     }
 
     public static void processRound() throws GameActionException {
-        RobotInfo[] nearbyRobots = robotController.senseNearbyRobots(-1, robotController.getTeam().opponent());
-        if (nearbyRobots.length >= 8 && robotController.getRoundNum() - lastPoliSpawn >= 1 + (nearbyRobots.length / 8)) {
-            lastPoliSpawn = robotController.getRoundNum();
+        MapLocation myLocation = robotController.getLocation();
+        RobotInfo[] enemyRobots = robotController.senseNearbyRobots(myLocation, -1, robotController.getTeam().opponent());
 
-            int influenceSum = 0;
-            for (int i = nearbyRobots.length; --i >= 0; ) {
-                influenceSum += nearbyRobots[i].conviction + 1;
-            }
-            build(RobotType.POLITICIAN, influenceSum + 10);
-        } else {
-            build(RobotType.MUCKRAKER, 1);
+        if (enemyRobots.length > 20) {
+            robotController.resign();
+            return;
         }
-        vote();
+
+        RobotInfo[] myRobots = robotController.senseNearbyRobots(-1, robotController.getTeam());
+        int poliCount = 0, slandererCount = 0;
+        int flag = robotController.getFlag(robotController.getID());
+        for (int i = myRobots.length; --i >= 0; ) {
+            if (myRobots[i].getType() == RobotType.POLITICIAN) poliCount++;
+            else if (myRobots[i].getType() == RobotType.SLANDERER) slandererCount++;
+            flag |= robotController.getFlag(myRobots[i].getID());
+        }
+        flag &= (1 << 18) - 1;
+        final int bubbleSize = Math.max(32, poliCount);
+        flag |= bubbleSize << 18;
+        robotController.setFlag(flag);
+        int nearestEnemy = enemyRobots.length > 0 ? myLocation.distanceSquaredTo(enemyRobots[0].getLocation()) : 1001001;
+//        int avgEnemy = 10;
+//        if (enemyRobots.length > 0) {
+//            avgEnemy = 0;
+//            for (int i = enemyRobots.length; --i >= 0;) {
+//                avgEnemy += enemyRobots[i].getConviction();
+//            }
+//            avgEnemy /= enemyRobots.length;
+//            avgEnemy++;
+//        }
+//        if (muckCount < 6 && build(RobotType.MUCKRAKER, 1) > 0) {
+//            muckCount++;
+//        } else
+        if (robotController.getRoundNum() <= 8 && nearestEnemy > bubbleSize) {
+            build(RobotType.SLANDERER, 63);
+        } else if (poliCount < 8) {
+            build(RobotType.POLITICIAN, 16);
+        } else if (slandererCount < Math.sqrt(bubbleSize) && nearestEnemy > bubbleSize) {
+            build(RobotType.SLANDERER, 63);
+        } else {
+            build(RobotType.POLITICIAN, 16);
+
+//            if (enemyRobots.length >= 8 && robotController.getRoundNum() - lastPoliSpawn >= 1 + (enemyRobots.length / 8)) {
+//                lastPoliSpawn = robotController.getRoundNum();
+//
+//                int influenceSum = 0;
+//                for (int i = enemyRobots.length; --i >= 0; ) {
+//                    influenceSum += enemyRobots[i].conviction + 1;
+//                }
+//                build(RobotType.POLITICIAN, influenceSum + 10);
+//            } else if (20 <= robotController.getRoundNum() && robotController.getRoundNum() <= 200) {
+//                build(RobotType.MUCKRAKER, 1);
+//            } else {
+//                build(RobotType.SLANDERER, 63);
+//            }
+        }
+        if (robotController.getRoundNum() >= 150 || (robotController.getRoundNum() >= 50 && robotController.getRoundNum() % 10 == 0)) {
+            vote();
+        }
     }
 
     /**
@@ -66,9 +113,8 @@ public class EnlightenmentCenter {
         if (vote > 2 && FastRandom.nextInt(15) == 0) {
             vote -= 2;
         }
-        build(RobotType.MUCKRAKER, 1);
         voted = false;
-        if (robotController.getInfluence() >= Math.max(50, vote) && robotController.getTeamVotes() < 750
+        if (robotController.getInfluence() >= Math.max(350, vote) && robotController.getTeamVotes() < 750
             /*&&FastRandom.nextInt(1500-robotController.getRoundNum())<(750-robotController.getTeamVotes())/0.7*/) {
             voted = true;
             lastVoteCount = robotController.getTeamVotes();
--- src/antimuckrush/Slanderer.java
+++ src/antimuckrush/Slanderer.java
@@ -1,16 +1,107 @@
 package antimuckrush;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
+
+import java.util.Arrays;
 
 public class Slanderer {
-	public static RobotController robotController;
+    public static RobotController robotController;
+    private static MapLocation startLocation;
 
-	public static void initialize(RobotController robotController) {
-		Slanderer.robotController = robotController;
-	}
+    public static void initialize(RobotController robotController) {
+        Slanderer.robotController = robotController;
+        RobotInfo[] myRobots = robotController.senseNearbyRobots(-1, robotController.getTeam());
+        for (int i=myRobots.length; --i >= 0; ) {
+            if (myRobots[i].getType() == RobotType.ENLIGHTENMENT_CENTER) {
+                startLocation = myRobots[i].getLocation();
+                break;
+            }
+        }
+    }
 
-	public static void processRound() throws GameActionException {
+    public static void processRound() throws GameActionException {
+        RobotInfo[] enemyRobots = robotController.senseNearbyRobots(-1, robotController.getTeam().opponent());
+        RobotInfo[] myRobots = robotController.senseNearbyRobots(-1, robotController.getTeam());
+        MapLocation curLocation = robotController.getLocation();
+        if (!robotController.isReady()) {
+            int flag = robotController.getFlag(robotController.getID());
+            for (int i = enemyRobots.length; --i >= 0; ) {
+                flag |= 1 << indexOfDirections(curLocation.directionTo(enemyRobots[i].getLocation()));
+            }
+            for (int i = myRobots.length; --i >= 0; ) {
+                flag |= robotController.getFlag(myRobots[i].getID());
+            }
+            robotController.setFlag(flag);
+            return;
+        }
 
-	}
+        int[] dirScore = new int[8];
+//        for (int i = enemyRobots.length; --i >= 0; ) {
+//            if (enemyRobots[i].getType() != RobotType.MUCKRAKER) continue;
+//
+//            Direction dir = curLocation.directionTo(enemyRobots[i].getLocation());
+//            for (int j = 8; --j >= 0; ) {
+//                if (dir.equals(Direction.allDirections()[j])) {
+//                    dirScore[j] = 2;
+//                } else if (dir.rotateLeft().equals(Direction.allDirections()[j])) {
+//                    dirScore[j] = 2;
+//                } else if (dir.rotateRight().equals(Direction.allDirections()[j])) {
+//                    dirScore[j] = 2;
+//                } else if (dir.rotateLeft().rotateLeft().equals(Direction.allDirections()[j])) {
+//                    dirScore[j] = Math.max(dirScore[j], 1);
+//                } else if (dir.rotateRight().rotateRight().equals(Direction.allDirections()[j])) {
+//                    dirScore[j] = Math.max(dirScore[j], 1);
+//                }
+//            }
+//        }
+//        for (int i = myRobots.length; --i >= 0; ) {
+//            switch (myRobots[i].getType()) {
+//                case MUCKRAKER:
+//                    break;
+//                default:
+//                    if (robotController.canGetFlag(myRobots[i].getID())) {
+//                        int flag = robotController.getFlag(myRobots[i].getID());
+//                        for (int j = 8; --j >= 0; ) {
+//                            if ((flag & (1 << j)) > 0) {
+//                                dirScore[(j + 6) % 8] = Math.max(dirScore[(j + 6) % 8], 1);
+//                                dirScore[(j + 7) % 8] = 2;
+//                                dirScore[j % 8] = 2;
+//                                dirScore[(j + 1) % 8] = 2;
+//                                dirScore[(j + 2) % 8] = Math.max(dirScore[(j + 2) % 8], 1);
+//                            }
+//                        }
+//                    }
+//            }
+//        }
+
+        if (enemyRobots.length > 0 || FastRandom.nextInt(4) == 0) {
+            double INF = 1e8;
+//            int minScore = 10;
+            boolean moveInwards = curLocation.distanceSquaredTo(startLocation) > 4;
+            Direction inwards = curLocation.directionTo(startLocation);
+            Direction moveDirection = Direction.CENTER;
+            double min = INF;
+            for (int i = 8; --i >= 0; ) {
+                final Direction cur = Direction.allDirections()[i];
+                if (robotController.canMove(cur)) {
+                    MapLocation location = robotController.adjacentLocation(Direction.allDirections()[i]);
+                    final double cost = (1.6 / robotController.sensePassability(location)) + FastRandom.nextInt(50) / 10.0;
+                    if (min > cost && (!moveInwards || cur.equals(inwards) || cur.equals(inwards.rotateLeft()) || cur.equals(inwards.rotateRight()))) {
+                        min = cost;
+                        moveDirection = cur;
+                    }
+                }
+            }
+            if (min + 1 < INF) {
+                robotController.move(moveDirection);
+            }
+        }
+    }
+
+    private static int indexOfDirections(Direction dir) {
+        for (int i = 9; --i >= 0; ) {
+            if (Direction.allDirections()[i].equals(dir)) return i;
+        }
+        return -1;
+    }
 }
