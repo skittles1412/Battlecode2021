1bc1cd5583b9dcef18d12c2192506098078ba7b1
qpwoeirut <stanleyzhong8@gmail.com>
2021-01-18 12:10:35 -0800

    add scraping for team and map wins/losses

--- src/scraping/scrape_scrim.py
+++ src/scraping/scrape_scrim.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from time import sleep
 
 from selenium import webdriver
@@ -7,7 +8,7 @@ from selenium.webdriver.common.by import By
 CREDENTIALS = open("credentials.txt", "r").readlines()
 USERNAME = CREDENTIALS[0].strip()
 PASSWORD = CREDENTIALS[1].strip()
-PAGES = 1
+PAGES = 10
 
 
 class DashboardScraper:
@@ -60,18 +61,18 @@ class DashboardScraper:
             assert len(cols) == 7
 
             time = cols[0].text + ' ' + cols[1].text
-            if cols[2].text in ("Rejected", "Queued", "Pending"):
+            if cols[2].text in ("Rejected", "Error", "Pending"):
                 continue
             score = cols[3].text
             opp = cols[4].text
             rank = cols[5].text
-            linkedTime = time
+            linked_time = time
             links.append("N/A")
             if cols[6].text != "N/A":
                 links[-1] = cols[6].find_element(By.CSS_SELECTOR, 'a').get_attribute("href")
-                linkedTime = "EQUALSHYPERLINK(\"" + links[-1] + "\"COMMA\"" + time + "\")"
+                linked_time = "EQUALSHYPERLINK(\"" + links[-1] + "\"COMMA\"" + time + "\")"
 
-            elems = [linkedTime, opp, score if len(score) > 3 else "N/A", rank]
+            elems = [linked_time, opp, score if len(score) > 3 else "N/A", rank]
             scrims.append(','.join(elems))
         return scrims, links
 
@@ -93,6 +94,9 @@ class MatchScraper:
             self.browser.find_element(By.XPATH, '//button[contains(@class, "modebutton")][text()="Queue"]').click()
             sleep(0.1)
 
+        teams = self.browser.find_element(By.CSS_SELECTOR, "span.red").text.strip(), self.browser.find_element(By.CSS_SELECTOR, "span.blue").text.strip()
+        opponent = teams[0] if teams[0] != "java :ghosthug:" else teams[1]
+
         maps = []
         durations = []
         winners = []
@@ -102,9 +106,9 @@ class MatchScraper:
             duration = info.strip().split()[-2]
             maps.append(round_map.strip())
             durations.append(duration.strip())
-            winners.append(info.split('-', maxsplit=1)[1].rsplit(" wins ", maxsplit=1)[0])
+            winners.append(info.split('-', maxsplit=1)[1].rsplit(" wins ", maxsplit=1)[0].strip())
 
-        return maps, durations, winners
+        return opponent, maps, durations, winners
 
 
 def main():
@@ -112,8 +116,17 @@ def main():
     browser = webdriver.Chrome()
 
     output = []
+    team_record = defaultdict(lambda: [0, 0])
+    map_record = defaultdict(lambda: [0, 0])
     for scrim, link in zip(scrims, links):
-        maps, durations, winners = MatchScraper(browser, link).run()
+        if link.startswith("https://"):
+            opponent, maps, durations, winners = MatchScraper(browser, link).run()
+            for m, d, w in zip(maps, durations, winners):
+                idx = w != "java :ghosthug:"
+                team_record[opponent][idx] += 1
+                map_record[m][idx] += 1
+        else:
+            maps, durations, winners = [["N/A"] * 3] * 3
         output.append(scrim)
         for i in range(3):
             output.append(f"Round {i+1},{maps[i]},{durations[i]},{winners[i]}")
@@ -122,6 +135,11 @@ def main():
     browser.quit()
 
     print('\n'.join(output))
+    print("\n\n")
+    for team, record in team_record.items():
+        print(f"{team},{record[0]},{record[1]}")
+    for m, record in map_record.items():
+        print(f"{m},{record[0]},{record[1]}")
 
 
 if __name__ == '__main__':
